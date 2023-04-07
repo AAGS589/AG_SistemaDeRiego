9f5a616dfb599c9c1ab606900a582cbc9de806d6
Alonso Anselmo Gómez Sánchez <68868495+AAGS589@users.noreply.github.com>
2023-04-06 21:07:09 -0600

    se agregaron nuevos parametros de entrada

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import random
 import itertools
 import csv
+import os
 from random import sample
 import math
 import matplotlib.pyplot as plt
@@ -15,14 +16,15 @@ class AG(QMainWindow):
     def __init__(self):
         super().__init__()
         uic.loadUi("style.ui",self)
-        # ... (Widgets existentes)
+        
         self.arboles_plantados = None
         self.mejor_individuo = None
+        self.penalizacion_cruces = 0.1
 
-        self.input_pobInicial.setText("50")   
-        self.input_pobMaxima.setText("200")
-        self.input_numGeneraciones.setText("2000")
-        self.input_presicion.setText("0.99")
+        self.input_pobInicial.setText("20")   
+        self.input_pobMaxima.setText("25")
+        self.input_numGeneraciones.setText("50")
+        self.input_presicion.setText("0.1")
         self.input_probCruza.setText("0.01")
         self.input_probMutacionIndividuo.setText("0.25")
         self.input_probMutacionGen.setText("0.99")
@@ -32,7 +34,7 @@ class AG(QMainWindow):
         self.ladoY = self.dimensionesTerreno / self.ladoX
         self.manguera = (0,0)
 
-        # Botones
+       
         self.btn_iniciarAlgoritmo.clicked.connect(self.iniciarAlgoritmo)
         self.btn_verPlantacion.clicked.connect(self.verPlantacion)
         self.btn_verMangueraRiego.clicked.connect(self.verMangueraRiego)
@@ -54,19 +56,19 @@ class AG(QMainWindow):
             print("Error: Por favor, ingrese valores válidos para todos los parámetros.")
             return
        
-        # Generar lista de árboles plantados en el terreno
+        
         self.arboles_plantados = self.generar_arboles_plantados()
 
-        # Generar población inicial
+      
         poblacion_inicial = self.generar_poblacion_inicial(self.arboles_plantados, self.pobInicial)
 
-        # Ejecutar algoritmo genético
+        
         self.mejor_individuo = self.algoritmo_genetico(poblacion_inicial, self.arboles_plantados)
 
-        # Calcular la aptitud del mejor individuo
+        
         aptitud_mejor_individuo = self.funcion_aptitud(self.mejor_individuo, self.arboles_plantados)
            
-        # Mostrar resultados
+       
         print("Mejor recorrido de manguera:", self.mejor_individuo)
         print("Aptitud del mejor recorrido de manguera:", aptitud_mejor_individuo)
 
@@ -83,10 +85,6 @@ class AG(QMainWindow):
         else:
             print("La plantación y/o el recorrido de la manguera no han sido generados todavía.")
     
-    def distancia_minima(self, tipo1, tipo2):
-        radios_copas = {"Mango": 7.5, "Limon": 5, "Nanche": 5, "Aguacate": 6, "Coco": 9}
-        return radios_copas[tipo1] + radios_copas[tipo2]
-
     def arbol_valido(self, nuevo_arbol, arboles_plantados):
         x1, y1, tipo1 = nuevo_arbol
         for arbol in arboles_plantados:
@@ -95,6 +93,11 @@ class AG(QMainWindow):
             if distancia < self.distancia_minima(tipo1, tipo2):
                 return False
         return True
+    
+    def distancia_minima(self, tipo1, tipo2):
+        radios_copas = {"Mango": 7.5, "Limon": 5, "Nanche": 5, "Aguacate": 6, "Coco": 9}
+        return radios_copas[tipo1] + radios_copas[tipo2]
+
 
     def arbol_mas_cercano(self, manguera, arboles):
         arbol_cercano = None
@@ -108,20 +111,19 @@ class AG(QMainWindow):
                 
         return arbol_cercano
 
-
     def generar_arboles_plantados(self, archivo="arboles2.csv"):
         arboles_plantados = []
 
         with open(archivo, "r") as f:
             reader = csv.reader(f)
-            next(reader)  # Saltar la fila del encabezado (Tipo, X, Y)
+            next(reader)  
             for row in reader:
                 tipo, x, y = row
                 arbol = (float(x), float(y), tipo)
 
-                # Validar que el árbol esté dentro de los límites del terreno
+                
                 if 0 <= arbol[0] < self.ladoX and 0 <= arbol[1] < self.ladoY:
-                    # Validar que el árbol cumpla con la distancia mínima
+                    
                     if self.arbol_valido(arbol, arboles_plantados):
                         arboles_plantados.append(arbol)
                     else:
@@ -155,15 +157,13 @@ class AG(QMainWindow):
         plt.ylabel("Eje Y")
         plt.title("Distribución de árboles plantados")
 
-        # Ajusta la leyenda para que se adapte a la ventana
+        
         plt.legend(loc="upper left", bbox_to_anchor=(1.05, 1), borderaxespad=0)
 
         plt.tight_layout()
         plt.show()
 
 
-    # Esta función debe generar una población inicial de individuos,
-    # donde cada individuo es una permutación aleatoria de la lista de árboles.
     def generar_poblacion_inicial(self, arboles_plantados, pobInicial):
         print("---------------------------INICIO METODO POBLACION INICIAL---------------------------")
         poblacion = []
@@ -176,7 +176,7 @@ class AG(QMainWindow):
         print("---------------------------FIN    METODO POBLACION INICIAL---------------------------")
         return poblacion
 
-        # La función debe devolver el mejor individuo (recorrido de manguera) encontrado.
+        
     def algoritmo_genetico(self, poblacion, arboles_plantados):
         mejor_individuo = None
         mejor_aptitud = float('inf')
@@ -185,10 +185,10 @@ class AG(QMainWindow):
 
         generacion = 0
         while generacion < self.numGeneraciones and mejora_aptitud >= self.precision:
-            # Calcular las aptitudes de todos los individuos en la población
+           
             aptitudes = [self.funcion_aptitud(individuo, arboles_plantados) for individuo in poblacion]
 
-            # Actualizar el mejor individuo si se encuentra uno mejor en la población actual
+           
             indice_mejor_individuo = aptitudes.index(min(aptitudes))
             aptitud_mejor_individuo = aptitudes[indice_mejor_individuo]
 
@@ -199,37 +199,36 @@ class AG(QMainWindow):
             
             self.mejor_aptitud_por_generacion.append(mejor_aptitud)
 
-            # Seleccionar individuos para la cruza y la mutación
+           
             individuos_seleccionados = self.seleccion(poblacion, aptitudes)
 
-            # Crear una nueva población cruzando y mutando a los individuos seleccionados
+         
             nueva_poblacion = []
 
             while len(nueva_poblacion) < len(poblacion):
-                # Seleccionar dos padres al azar de los individuos seleccionados
+                
                 padre1 = random.choice(individuos_seleccionados)
                 padre2 = random.choice(individuos_seleccionados)
 
-                # Cruzar los padres para generar un hijo
+              
                 hijo = self.cruza(padre1, padre2)
 
-                # Aplicar mutación al hijo con una cierta probabilidad
+              
                 if random.random() < self.probMutacionIndividuo:
                     hijo = self.mutacion(hijo)
 
-                # Agregar el hijo a la nueva población
+               
                 nueva_poblacion.append(hijo)
 
             poblacion = self.poda(nueva_poblacion)
-            # Reemplazar la población antigua con la nueva población
+          
             poblacion = nueva_poblacion
 
             generacion += 1
         
         return mejor_individuo
 
-        # Esta función debe calcular la suma de las distancias entre árboles
-        # adyacentes en el recorrido de la manguera.
+
     def funcion_aptitud(self, individuo, arboles_plantados):
         def distancia(arbol1, arbol2):
             x1, y1, *_ = arbol1
@@ -238,23 +237,22 @@ class AG(QMainWindow):
             return ((x1 - x2) ** 2 + (y1 - y2) ** 2) ** 0.5
 
 
-        # Agregar la posición de la bomba de agua
-        bomba_de_agua = (self.ladoX / 2, 0)
+      
+        bomba_de_agua = (0,0)
 
-        # Distancia desde la bomba de agua al primer árbol
+       
         distancia_total = distancia(bomba_de_agua, arboles_plantados[individuo[0]])
 
-        # Distancia entre árboles adyacentes
+       
         distancia_total += sum(distancia(arboles_plantados[individuo[i]], arboles_plantados[individuo[i + 1]])
                                 for i in range(len(individuo) - 1))
 
-        # Distancia entre el último árbol y la bomba de agua
+        
         distancia_total += distancia(arboles_plantados[individuo[-1]], bomba_de_agua)
 
         return distancia_total
 
-        # Esta función debe seleccionar individuos de la población para la cruza
-        # y mutación basándose en sus aptitudes (p. ej., usando selección por torneo).
+        
     def seleccion(self, poblacion, aptitudes, k=2):
         seleccionados = []
 
@@ -265,24 +263,24 @@ class AG(QMainWindow):
         
         return seleccionados
 
-    # Implementa un enfoque de cruza ordenada (OX) para este problema.
+    
     def cruza(self, padre1, padre2):
-        # Obtener dos puntos de corte aleatorios
+       
         punto_corte1 = random.randint(0, len(padre1) - 1)
         punto_corte2 = random.randint(0, len(padre1) - 1)
 
-        # Asegurar que punto_corte1 <= punto_corte2
+       
         if punto_corte1 > punto_corte2:
             punto_corte1, punto_corte2 = punto_corte2, punto_corte1
 
-        # Copiar la sección intermedia del primer padre al hijo
+      
         hijo = [None] * len(padre1)
         hijo[punto_corte1:punto_corte2] = padre1[punto_corte1:punto_corte2]
 
-        # Copiar los elementos restantes del segundo padre al hijo, manteniendo el orden relativo
+        
         elementos_faltantes = [x for x in padre2 if x not in hijo[punto_corte1:punto_corte2]]
 
-        # Colocar los elementos faltantes en las posiciones correctas del hijo
+       
         for i in range(len(hijo)):
             if hijo[i] is None:
                 hijo[i] = elementos_faltantes.pop(0)
@@ -293,11 +291,11 @@ class AG(QMainWindow):
 
     def mutacion(self, individuo):
      
-        # Seleccionar dos posiciones aleatorias dentro del individuo
+        
         pos1 = random.randint(0, len(individuo) - 1)
         pos2 = random.randint(0, len(individuo) - 1)
 
-        # Intercambiar los elementos en las posiciones seleccionadas
+       
         individuo[pos1], individuo[pos2] = individuo[pos2], individuo[pos1]
         return individuo
     
@@ -313,7 +311,7 @@ class AG(QMainWindow):
         fig, ax = plt.subplots(figsize=(10, 6))
         colores = {"Mango": "red", "Limon": "green", "Nanche": "blue", "Aguacate": "yellow", "Coco": "purple"}
 
-        # Crear una lista de elementos de leyenda
+        
         elementos_leyenda = []
 
         for tipo, color in colores.items():
@@ -322,10 +320,10 @@ class AG(QMainWindow):
         for x, y, tipo in arboles_plantados:
             plt.scatter(x, y, color=colores[tipo], marker='o', s=50)
 
-        # Dibujar la ruta de la manguera
+       
         ruta = [arboles_plantados[i] for i in mejor_individuo]
 
-        # Añadir el árbol más cercano al inicio de la ruta
+        
         arbol_cercano = self.arbol_mas_cercano(manguera, arboles_plantados)
         ruta.insert(0, arbol_cercano)
 
@@ -334,19 +332,19 @@ class AG(QMainWindow):
 
         plt.plot(ruta_x, ruta_y, color="black", linewidth=1, linestyle='--')
 
-        # Dibujar la conexión entre la bomba y el árbol más cercano
+       
         plt.plot([manguera[0], arbol_cercano[0]], [manguera[1], arbol_cercano[1]], color="black", linewidth=1, linestyle="--")
 
         plt.scatter(*manguera, color="black", marker='s', s=50)
         plt.text(manguera[0], manguera[1], "Bomba", fontsize=12)
 
-        # Dibujar las flechas en la dirección de la manguera
+       
         for i in range(len(ruta) - 1):
             x1, y1, _ = ruta[i]
             x2, y2, _ = ruta[i + 1]
             plt.arrow(x1, y1, x2 - x1, y2 - y1, color="black", linestyle='--', length_includes_head=True, head_width=2, head_length=2)
 
-        # Marcar el final de la manguera
+        
         ultimo_arbol = ruta[-1]
         plt.scatter(ultimo_arbol[0], ultimo_arbol[1], color="orange", marker="*", s=100)
         plt.text(ultimo_arbol[0], ultimo_arbol[1], "Fin manguera", fontsize=12)
@@ -354,7 +352,7 @@ class AG(QMainWindow):
         plt.xlim(0, self.ladoX)
         plt.ylim(0, self.ladoY)
 
-        # Añadir la leyenda
+        
         plt.legend(handles=elementos_leyenda, loc="upper left", bbox_to_anchor=(1.05, 1), borderaxespad=0)
         plt.title("Plantación y recorrido de la manguera de riego")
         plt.tight_layout()
